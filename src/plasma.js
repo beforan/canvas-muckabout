d68d6b8459721cb1c7b131dc37909810e2d60f6d
Jonathan Couldridge <jcouldridge@outlook.com>
2018-10-03 09:05:01 +0100

    wip plasma effect

--- src/plasma.js
+++ src/plasma.js
@@ -1,3 +1,4 @@
+import HslToRgb from "./hsl-to-rgb";
 import * as ImageDataHelpers from "./imagedata-helpers";
 
 const canvas = document.getElementById("gameScreen");
@@ -6,17 +7,39 @@ const context = canvas.getContext("2d");
 const w = canvas.width;
 const h = canvas.height;
 
+// build a 256 colour palette that covers a full hue range
+const palette = [];
+for (let i = 0; i < 256; i++) {
+  palette[i] = HslToRgb(i, 0.5, 0.5); //max S and L
+}
+let paletteShift = 0;
+
+// we use a plasma buffer to do math on, which references palette indices
+const plasma = [];
+
 const tick = () => {
   const imageData = context.createImageData(w, h);
 
-  for (let y = 0; y < w; y++) {
-    for (let x = 0; x < h; x++) {
-      const c = 128 + 128 * Math.sin(x / 8);
-      ImageDataHelpers.setPixel(imageData, x, y, { r: c, g: c, b: c, a: 255 });
+  for (let y = 0; y < h; y++) {
+    plasma[y] = plasma[y] || [];
+    for (let x = 0; x < w; x++) {
+      // here we actually calculate shit to get colours based on pixel position
+      plasma[y][x] = Math.floor(
+        128 +
+          (128 * Math.tan(x / (w - 10))) / Math.cos(paletteShift) +
+          (128 + (128 * Math.tan(y / (h - 10))) / Math.sin(paletteShift))
+      );
+
+      // while we're here, set the actual pixel to the indexed palette value
+      const [r, g, b] = palette[(plasma[y][x] + paletteShift * 150) & 255];
+      ImageDataHelpers.setPixel(imageData, x, y, { r, g, b, a: 255 });
     }
   }
 
-  //tick again
+  context.putImageData(imageData, 0, 0);
+
+  // tick again
+  paletteShift += 0.1;
   requestAnimationFrame(tick);
 };
 
